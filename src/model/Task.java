d38d801aadecaf0ae4a046406970029eb8ce080e
Darya Sozinova <sozinova.da@gmail.com>
2025-12-17 14:53:06 +0400

    implement Task model

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,4 +1,61 @@
 package model;
 
+import java.util.Objects;
+
 public class Task {
+     private int id;
+     private String name;
+     private String description;
+     private Status status;
+
+     public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+     }
+
+     public void setId(int id) {
+        if (this.id == 0 && id > 0) {
+            this.id = id;
+        }
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public int getId() {
+         return id;
+     }
+
+     public String getDescription() {
+         return description;
+     }
+
+     public Status getStatus() {
+         return status;
+     }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "status=" + status +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
