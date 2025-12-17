52aad87cc8944f4e5291d9d06a13a6787eb8bc5c
Darya Sozinova <sozinova.da@gmail.com>
2025-12-17 18:37:38 +0400

    implement subtask model

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,4 +1,14 @@
 package model;
 
-public class Subtask {
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
 }
