1cc1263dc64ed20060f1cd349d8e229674ded544
Darya Sozinova <sozinova.da@gmail.com>
2025-12-17 18:40:37 +0400

    add TaskManager skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import model.Status;
+import model.Task;
+import model.Subtask;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
     }
 }
