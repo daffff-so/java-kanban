17ea98f37ef45321c595fe72a52c08fbd513948f
Darya Sozinova <sozinova.da@gmail.com>
2025-12-17 18:38:15 +0400

    implement Epic model

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,4 +1,31 @@
 package model;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Epic {
+public class Epic extends Task{
+    private List<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description, Status.NEW);
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        if (!subtaskIds.contains(subtaskId)) {
+        } else {
+            subtaskIds.add(subtaskId);
+        }
+    }
+
+    public void deleteSubtaskId(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    public List<Integer> getIdList() {
+        return new ArrayList<>(subtaskIds);
+    }
+
+    public void clearList() {
+         subtaskIds.clear();
+    }
+
 }
