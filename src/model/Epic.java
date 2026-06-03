42b95274f5e504f7ab24a722174282908204c9fa
Darya Sozinova <sozinova.da@gmail.com>
2026-06-03 14:10:58 +0400

    Fix code style

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -3,7 +3,6 @@ package model;
 import java.util.List;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
-import java.time.Duration;
 
 public class Epic extends Task {
     private List<Integer> subtaskIds = new ArrayList<>();
